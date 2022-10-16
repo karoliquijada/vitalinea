f5a22ad25e27b8497d45d68a86dacc5d82d09cff
karoliquijada <kquijada9894@gmail.com>
2022-10-16 14:46:46 -0400

    header media queries

--- src/containers/header/header.container.jsx
+++ src/containers/header/header.container.jsx
@@ -11,30 +11,33 @@ const Header = () => {
   return (
     <header id="header" className="header">
       <div className="header__bg"></div>
+
       <div className="header__img">
         <img src={headerImage} alt="bebida fresa" />
       </div>
 
-      <div className="header__flavors">
-        <span>Sabores:</span>
-        <span className="header__flavors--flavor">Fresa</span>
-        <span className="header__flavors--flavor">Guayaba</span>
-        <span className="header__flavors--flavor">Toronja</span>
-      </div>
+      <div className="header__content">
+        <div className="header__flavors">
+          <span>Sabor:</span>
+          <span className="header__flavors--flavor">Fresa</span>
+          <span className="header__flavors--flavor">Guayaba</span>
+          <span className="header__flavors--flavor">Toronja</span>
+        </div>
 
-      <div className="header__text">
-        <h1 className="header__text--title">Vitalínea® Bebible Fresa 217 gr.</h1>
-        <p className="header__text--description">
-          ¡La presentación ideal para llevar contigo! Vitalínea bebible es la opción si eres de las personas que siempre están activas y
-          quieren probar algo sano, rico y práctico.
-        </p>
-      </div>
-      
-      <Button>Comprar en Walmart</Button>
+        <div className="header__text">
+          <h1 className="header__text--title">Vitalínea® Bebible Fresa 217 gr.</h1>
+          <p className="header__text--description">
+            ¡La presentación ideal para llevar contigo! Vitalínea bebible es la opción si eres de las personas que siempre están activas y
+            quieren probar algo sano, rico y práctico.
+          </p>
+        </div>
 
-      <div className="header__stamps">
-        <img src={stampSello} alt="sello" />
-        <img src={stampCalorias} alt="calorias" />
+        <Button>Comprar en Walmart</Button>
+
+        <div className="header__stamps">
+          <img src={stampSello} alt="sello" />
+          <img src={stampCalorias} alt="calorias" />
+        </div>
       </div>
 
       <div className="header__arrow">
